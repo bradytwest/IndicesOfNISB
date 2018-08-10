4223ee396009803c6a013f219d4d47c37875ce03
Phil Boonstra <pbgamer@rocketmail.com>
2018-08-10 14:11:36 -0400

    Split nisb into nisb and nisb_regress, to differentiate between scenarios in which the proxy variable X is in hand and the proxy variable X is itself a prediction from an administrative proxy Z. Also removed some extraneous variables that are not needed for nisb, changed 'Xbar' variables to 'mean_X' variables, and fixed a small mistake in the calculation of the administrative statistics that miscounted the denominator of the means by 7.

--- nisb_example.R
+++ nisb_example.R
@@ -1,8 +1,10 @@
 ############################
 # EXAMPLE APPLICATION: NSFG
 ############################
+library(mnormt);library(MCMCpack);library(ggplot2);library(nlme);library(magrittr);
 
-source("https://github.com/bradytwest/IndicesOfNISB/raw/master/nisb_V6.R");
+
+source("https://github.com/bradytwest/IndicesOfNISB/raw/master/nisb_functions.R");
 
 # Set data path, including the name of the input .csv file (with variable names in header).
 data_path = "https://github.com/bradytwest/IndicesOfNISB/raw/master/fem_comp.csv";
@@ -18,15 +20,15 @@ sampling_col = "smartphone";
 # data_selected includes the dependent variables; data_no_selected does not. 
 # This is the distinguishing characteristic between the two populations.
 # Just comment out the data_no_selected and admin_statistics_no_selected line if data are not available on non-selected cases.
-data_selected = as.dummy(data_all[which(data_all[,sampling_col]==1),c(depvar_col,predvar_cols)],transform_cols = predvar_cols_as_factors);
-data_no_selected = as.dummy(data_all[which(data_all[,sampling_col]==0),predvar_cols],transform_cols = predvar_cols_as_factors);
+data_selected = as.dummy(data_all[which(data_all[,sampling_col]==1 & rowSums(is.na(data_all[,c(depvar_col,predvar_cols)]))==0),c(depvar_col,predvar_cols)],transform_cols = predvar_cols_as_factors);
+data_no_selected = as.dummy(data_all[which(data_all[,sampling_col]==0 & rowSums(is.na(data_all[,c(predvar_cols)]))==0),predvar_cols],transform_cols = predvar_cols_as_factors);
 
-admin_statistics_no_selected = list(mean_Z_no_selected = colMeans(data_no_selected,na.rm=T), 
-                                    var_Z_no_selected = var(data_no_selected,use="complete.obs"),
-                                    n0 = length(which(rowSums(is.na(data_no_selected))==0)));
-admin_statistics_selected = list(mean_YZ_selected = colMeans(data_selected,na.rm=T), 
-                                 var_YZ_selected = var(data_selected,use="complete.obs"),
-                                 n1 = length(which(rowSums(is.na(data_selected))==0)));
+admin_statistics_no_selected = list(mean_Z_no_selected = colMeans(data_no_selected), 
+                                    var_Z_no_selected = var(data_no_selected),
+                                    n0 = nrow(data_no_selected));
+admin_statistics_selected = list(mean_YZ_selected = colMeans(data_selected), 
+                                 var_YZ_selected = var(data_selected),
+                                 n1 = nrow(data_selected));
 
 # Set parameters for the nisb_bayes() and nisb() functions.
 random_seed = 1;
@@ -62,8 +64,8 @@ fit1$smub_summaries_conditional;
 #Nearly equal to the observed quantiles of SMUB when fixing phi at 0, 0.5, or 1 (as we would hope and expect)
 fit2$smub_summaries_conditional
 
-# Generate SMUB(0), SMUB(0.5), and SMUB(1) using respondent data only.
+# Generate SMUB(0), SMUB(0.5), and SMUB(1) using respondent data only. In this case, X is not directly observed
+# but is instead constructed from an initial regression of Y on Z, where Z is a multivariate administrative proxy. 
 # NOTE: foo$mean_X_pop can be replaced with the known mean for X from the target population.
-fit3 = nisb(admin_statistics_selected,fit1$mean_X_pop);
+fit3 = nisb(mean_X_pop = fit1$mean_X_pop, admin_statistics_selected = admin_statistics_selected);
 fit3$smub_point_est;
-
